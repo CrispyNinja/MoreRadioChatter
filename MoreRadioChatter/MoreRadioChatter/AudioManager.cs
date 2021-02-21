bd6c651b3eee44064c2255a57c230d1e78a61a29
CrispyNinja <eirik1@live.no>
2021-02-21 23:02:18 +0100

    Uploaded source

---
+++ MoreRadioChatter/MoreRadioChatter/AudioManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using Rage;
+using Rage.Native;
+using LSPD_First_Response.Mod.API;
+using System.Windows.Media;
+using System.Collections.Generic;
+using NAudio.Wave;
+
+namespace MoreRadioChatter
+{
+    public class AudioManager
+    {
+        string baseUrl = "lspdfr/audio/scanner/MoreRadioChatter Audio/";
+
+        InitializationFile initFile;
+        int volume;
+        
+        public AudioManager()
+        {
+            initFile = iniFile();
+
+            volume = int.Parse(initFile.ReadString("Settings", "Volume", "15"));
+        }
+
+        private InitializationFile iniFile()
+        {
+            InitializationFile init = new InitializationFile("Plugins/LSPDFR/MoreRadioChatter.ini");
+            init.Create();
+            return init;
+        }
+
+        private string getLineUrl(string line)
+        {
+            return baseUrl + line + ".wav";
+        }
+
+        private string getLineDir(string line)
+        {
+            return baseUrl + line;
+        }
+
+        public void PlayLine(string line)
+        {
+            // Split the line to get every file from string
+            String[] lines = line.Split(' ');
+
+            GameFiber.Yield(); // idk
+
+            foreach (string file in lines)
+            {
+                string dirUrl = getLineDir(file);
+
+                try
+                {
+                    PlaybackEngine eng = new PlaybackEngine(44100, 2);
+
+                    if (!Directory.Exists(dirUrl)) // Is it a file?
+                    {
+                        string url = getLineUrl(file);
+
+                        Game.LogTrivial("[MoreRadioChatter] Playing chatter '" + file + "'");
+                        eng.PlaySound(url, volume);
+                    }
+                    else // It is a folder
+                    {
+                        // Get every file in folder
+                        DirectoryInfo dir = new DirectoryInfo(dirUrl);
+                        FileInfo[] files = dir.GetFiles();
+                        // Get one random file from folder
+                        FileInfo rndFile = files[new Random().Next(0, files.Length - 1)];
+
+                        Game.LogTrivial("[MoreRadioChatter] Playing random file from folder, chatter '" + file + "'");
+                        eng.PlaySound(rndFile.FullName, volume);
+                    }
+
+                    while (eng.outputDevice.PlaybackState == PlaybackState.Playing)
+                    {
+                        GameFiber.Sleep(100);
+                    }
+                    Game.LogTrivial("[MoreRadioChatter] Play done");
+                }
+                catch(Exception e)
+                {
+                    Game.LogTrivial("[MoreRadioChatter] Something went wrong for file '" + file + "', see error below:");
+                    Game.LogTrivial("[MoreRadioChatter] ERROR: " + e.Message);
+                }
+            }
+        }
+
+    }
+}
---
+++ MoreRadioChatter/MoreRadioChatter/EntryPoint.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Rage;
+using Rage.Native;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Engine;
+using LSPD_First_Response.Mod.Callouts;
+
+[assembly: Rage.Attributes.Plugin("MoreRadioChatter", Description = "Want a more realistic experience while playing LSPDFR?", Author = "AkASlow")]
+namespace MoreRadioChatter
+{
+    public class EntryPoint : Plugin
+    {
+        private bool isAsleep;
+        private int lastIndex;
+
+        int sleepMin;
+        int sleepMax;
+        int currentLines;
+        bool onlyInVehicle;
+        bool onlyInPoliceVehicle;
+        bool playInPursuit;
+        InitializationFile initFile;
+        List<string> chatterLines;
+
+        private AudioManager audioManager;
+
+        private InitializationFile iniFile()
+        {
+            InitializationFile init = new InitializationFile("Plugins/LSPDFR/MoreRadioChatter.ini");
+            init.Create();
+            return init;
+        }
+
+        private List<String> chatterLinesFromIni()
+        {
+            List<string> list = new List<string>();
+
+            for (int i = 1; i < currentLines; i++)
+            {
+                list.Add(initFile.ReadString("Lines", "Chatter_" + i, "null"));
+            }
+
+            return list;
+        }
+
+        private void playRandomAudio()
+        {
+            if(isAsleep)
+            {
+                return;
+            }
+
+            // First sleep
+            isAsleep = true;
+            int sleepRnd = new Random().Next(sleepMin, sleepMax);
+            Game.LogTrivial("[MoreRadioChatter] Sleeping for " + sleepRnd + " seconds!");
+            GameFiber.Sleep(sleepRnd * 1000);
+
+            // Select random audio
+            int randomIndex = new Random().Next(1, chatterLines.Count);
+            if(chatterLines.Count > 1)
+            {
+                if(lastIndex != randomIndex)
+                {
+                    this.lastIndex = randomIndex;
+                }
+                else
+                {
+                    // Last played is same...
+                    if(randomIndex > 2 && chatterLines.Count > 2)
+                    {
+                        randomIndex--;
+                    }
+                    else if(randomIndex < 2 && chatterLines.Count > 2)
+                    {
+                        randomIndex++;
+                    }
+                    else
+                    {
+                        Game.LogTrivial("[MoreRadioChatter] Could not play chatter: '" + randomIndex + "'");
+                        isAsleep = false;
+                        return;
+                    }
+                }
+            }
+
+            // Get the player
+            Ped localPed = Game.LocalPlayer.Character;
+
+            // If the player is in a pursuit, don't play audio
+            if(!playInPursuit && Functions.IsPedInPursuit(localPed))
+            {
+                Game.LogTrivial("[MoreRadioChatter] Should have played, but player is in pursuit");
+                isAsleep = false;
+                return;
+            }
+
+            // Player must be in vehicle if we can play the chatter
+            if(onlyInVehicle)
+            {
+                if (localPed.CurrentVehicle != null)
+                {
+                    if(onlyInPoliceVehicle)
+                    {
+                        // Get the current vehicle to verify it is a police vehicle
+                        if (localPed.IsInAnyPoliceVehicle)
+                        {
+                            // Only in vehicle (must be police), play chatter
+                            Game.LogTrivial("[MoreRadioChatter] Playing: " + randomIndex);
+                            audioManager.PlayLine(chatterLines[randomIndex]);
+                        }
+                        else
+                        {
+                            Game.LogTrivial("[MoreRadioChatter] Should have played, but not in police vehicle");
+                        }
+                    }
+                    else
+                    {
+                        // Not only in police vehicle, play chatter
+                        Game.LogTrivial("[MoreRadioChatter] Playing: " + randomIndex);
+                        audioManager.PlayLine(chatterLines[randomIndex]);
+                    }
+                }
+                else
+                {
+                    Game.LogTrivial("[MoreRadioChatter] Should have played, but not in vehicle");
+                }
+            }
+            else
+            {
+                // Not only in vehicle, play chatter on foot as well
+                Game.LogTrivial("[MoreRadioChatter] Playing: " + randomIndex);
+                audioManager.PlayLine(chatterLines[randomIndex]);
+            }
+
+            isAsleep = false;
+        }
+
+        public EntryPoint()
+        {
+            GameFiber.StartNew((() =>
+                {
+                    while(true)
+                    {
+                        playRandomAudio();
+                        GameFiber.Yield();
+                    }
+                }
+            ));
+        }
+
+        public override void Initialize()
+        {
+            initFile = iniFile();
+
+            sleepMin = int.Parse(initFile.ReadString("Settings", "MinWait", "15"));
+            sleepMax = int.Parse(initFile.ReadString("Settings", "MaxWait", "45"));
+            currentLines = int.Parse(initFile.ReadString("Settings", "CurrentLines", "0"));
+
+            onlyInVehicle = initFile.ReadBoolean("Settings", "OnlyInVehicle", true);
+            onlyInPoliceVehicle = initFile.ReadBoolean("Settings", "OnlyInPoliceVehicle", true);
+            playInPursuit = initFile.ReadBoolean("Settings", "PlayInPursuit", false);
+
+            audioManager = new AudioManager();
+            chatterLines = chatterLinesFromIni();
+
+            Game.LogTrivial("[MoreRadioChatter] Plugin loaded!");
+        }
+
+        public override void Finally()
+        {
+
+        }
+    }
+}
